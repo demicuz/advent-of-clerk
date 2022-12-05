075345f107c963c57bc5c3728b2f5e8025d7d71f
Denis Mazurak <pure.demicube@gmail.com>
2022-12-05 04:20:54 +0300

    Day 4

--- src/advent_of_clerk/day_04.clj
+++ src/advent_of_clerk/day_04.clj
@@ -1,3 +1,39 @@
 ;; # 🎄 Advent of Clerk: Day 4
 (ns advent-of-clerk.day-04
-  (:require [nextjournal.clerk :as clerk]))
+  (:require
+    [nextjournal.clerk :as clerk]
+    [clojure.string :as str]))
+
+(defn parse-int [s]
+  (Integer/parseInt s))
+
+(def input (slurp "resources/day_04.txt"))
+
+(defn process-line [line]
+  (->> line
+       (re-matches #"(\d+)-(\d+),(\d+)-(\d+)")
+       (drop 1)
+       (map parse-int)))
+
+(def data
+  (->> input
+       str/split-lines
+       (map process-line)))
+
+(defn includes? [[x1 x2 y1 y2]]
+  (or
+   (and (<= x1 y1) (>= x2 y2))
+   (and (>= x1 y1) (<= x2 y2))))
+
+(->> data
+     (filter includes?)
+     count)
+
+(defn overlaps? [[x1 x2 y1 y2]]
+  (or
+   (and (<= x1 y2) (>= x2 y1))
+   (and (<= y1 x2) (>= y2 x1))))
+
+(->> data
+     (filter overlaps?)
+     count)
