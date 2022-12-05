6ad1f8d743208fc60034d08bebfe59509faf9568
Denis Mazurak <pure.demicube@gmail.com>
2022-12-05 04:16:56 +0300

    Day 3

--- src/advent_of_clerk/day_03.clj
+++ src/advent_of_clerk/day_03.clj
@@ -1,3 +1,43 @@
 ;; # 🎄 Advent of Clerk: Day 3
 (ns advent-of-clerk.day-03
-  (:require [nextjournal.clerk :as clerk]))
+  (:require
+    [nextjournal.clerk :as clerk]
+    [clojure.string :as str]
+    [clojure.set :as set]))
+
+(def input (slurp "resources/day_03.txt"))
+
+(def alphabet "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+(def scores (->> (range)
+                 (drop 1)
+                 (interleave alphabet)
+                 (apply hash-map)))
+
+(defn process-line [line]
+  (let [half-len (/ (count line) 2)]
+    (->> line
+         (split-at half-len)
+         (map set)
+         (apply set/intersection)
+         (first)
+         (scores))))
+
+(->> input
+     str/split-lines
+     (map process-line)
+     (reduce +)
+     )
+
+(defn process-three-lines [lines]
+  (->> lines
+       (map set)
+       (apply set/intersection)
+       (first)
+       (scores)))
+
+(->> input
+     str/split-lines
+     (partition 3)
+     (map process-three-lines)
+     (reduce +))
